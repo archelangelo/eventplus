cf60b016360c33e35053bab3e0966e29853599cc
lge33 <lge33@gatech.edu>
2019-12-22 21:27:24 -0500

    rest_ongoing

--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+class UserEvent(APIView):
+    def get(self, request):
+        pass
+    
+    def post(self, request):
+        pass
