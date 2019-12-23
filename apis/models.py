c0a030a86837e580f5d4d95de216c96fe2915cd0
Yiran Hu <medycc@gmail.com>
2019-12-23 01:55:08 -0500

    Changed model creation implementation

--- apis/models.py
+++ apis/models.py
@@ -1,8 +1,9 @@
 from django.contrib.gis.db import models
 from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
+from django.core.exceptions import FieldError
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.measure import D
+import datetime
 import requests
 import os
 
@@ -16,14 +17,15 @@ class Event(models.Model):
     place_id = models.CharField(max_length=100)
     place_name = models.CharField(max_length=100)
     location = models.PointField()
-    time = models.DateTimeField()
+    time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.place_name
 
-    def clean(self):
+    @classmethod
+    def create(cls, host=None, place_id='', time=datetime.datetime.now()):
         params = {
-            'place_id': self.place_id,
+            'place_id': place_id,
             'key': GOOGLE_API_KEY,
             'fields': 'name,place_id,geometry',
         }
@@ -31,9 +33,13 @@ class Event(models.Model):
             r = requests.get(url=GOOGLE_PLACE_DETAILS_URL, params=params)
         except requests.exceptions.RequestException as err:
             print(err)
-            raise ValidationError('Error getting response from Google API: {}'.format(err))
+            raise FieldError('Error getting response from Google API: {}'.format(err))
         if r.status_code != 200:
-            raise ValidationError('Error getting response from Google API: Status code {}'.format(r.status_code))
-        result = r.json()['result']
-        self.place_name = result['name']
-        self.location = GEOSGeometry('POINT({lng} {lat})'.format(**result['geometry']['location']), srid=SRID)
+            raise FieldError('Error getting response from Google API: Status code {}'.format(r.status_code))
+        r_json = r.json()
+        if r_json['status'] != 'OK':
+            raise FieldError('Error getting response from Google API: Response status {}'.format(r_json['status']))
+        result = r_json['result']
+        place_name = result['name']
+        location = GEOSGeometry('POINT({lng} {lat})'.format(**result['geometry']['location']), srid=SRID)
+        return cls(host=host, place_id=place_id, place_name=place_name, location=location, time=time)
