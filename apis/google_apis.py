59182653fefdbeef6d0379ddd3562a2e4da42817
Luwei Ge <rockingspore@gmail.com>
2019-12-23 21:09:08 -0500

    added textsearch and nearbysearch methods to the googleapi client; added a simple test for the detail_request

--- apis/google_apis.py
+++ apis/google_apis.py
@@ -4,8 +4,10 @@ from django.core.exceptions import FieldError
 
 class Client():
 
-    GOOGLE_API_KEY = os.environ['PLACES_API_KEY']
+    GOOGLE_API_KEY = os.getenv('PLACES_API_KEY', '')
     GOOGLE_PLACE_DETAILS_URL = 'https://maps.googleapis.com/maps/api/place/details/json'
+    GOOGLE_PLACE_NEARBY_URL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
+    GOOGLE_PLACE_TEXT_URL = 'https://maps.googleapis.com/maps/api/place/textsearch/json'
 
     def details_request(self, place_id = ''):
         params = {
@@ -15,6 +17,34 @@ class Client():
         }
         try:
             r = requests.get(url=Client.GOOGLE_PLACE_DETAILS_URL, params=params)
+        except requests.exceptions.RequestException as err:
+            print(err)
+            raise FieldError('Error getting response from Google API: {}'.format(err))
+        if r.status_code != 200:
+            raise FieldError('Error getting response from Google API: Status code {}'.format(r.status_code))
+        return r.json()
+
+    def nearby_request(self):
+        params = {
+            'key': Client.GOOGLE_API_KEY,
+            'fields': 'name,place_id,geometry',
+        }
+        try:
+            r = requests.get(url=Client.GOOGLE_PLACE_NEARBY_URL, params=params)
+        except requests.exceptions.RequestException as err:
+            print(err)
+            raise FieldError('Error getting response from Google API: {}'.format(err))
+        if r.status_code != 200:
+            raise FieldError('Error getting response from Google API: Status code {}'.format(r.status_code))
+        return r.json()
+
+    def textsearch_request(self):
+        params = {
+            'key': Client.GOOGLE_API_KEY,
+            'fields': 'name,place_id,geometry',
+        }
+        try:
+            r = requests.get(url=Client.GOOGLE_PLACE_TEXT_URL, params=params)
         except requests.exceptions.RequestException as err:
             print(err)
             raise FieldError('Error getting response from Google API: {}'.format(err))
--- apis/tests.py
+++ apis/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .google_apis import Client
 
-# Create your tests here.
+class GoogleAPITestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.place_id = 'ChIJN1t_tDeuEmsRUsoyG83frY4'
+    
+    def test_detail_request(self):
+        response_body = self.client.details_request(self.place_id)
+        self.assertIsNot(Client.GOOGLE_API_KEY, '')
+        self.assertEqual(response_body['status'], 'OK')
