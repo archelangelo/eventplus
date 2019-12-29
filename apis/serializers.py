8f8ffb02f0605e222ba6962fed6401b7a6d8364d
Yiran Hu <medycc@gmail.com>
2019-12-29 16:20:16 -0500

    Switched to using nest serializer for the host field in EventSerializer

--- apis/serializers.py
+++ apis/serializers.py
@@ -2,12 +2,14 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Event
 
-class EventSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Event
-        fields = ('id', 'host', 'place_id', 'place_name', 'location', 'time')
-
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'events')
+        fields = ('id', 'username')
+
+class EventSerializer(serializers.HyperlinkedModelSerializer):
+    host = UserSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Event
+        fields = ('id', 'host', 'place_id', 'place_name', 'location', 'time')
